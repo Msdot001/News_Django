ff8bfe72bbcbc903c16ee9389e17d57dd1c9186c
Moshood Owolabi <98819100+Msdot001@users.noreply.github.com>
2023-01-16 23:11:56 +0100

    add customUser model

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,10 +5,18 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ("age",)
+        fields = (
+            "username",
+            "email",
+            "age",            
+        )
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields =  (
+            "username",
+            "email",
+            "age",            
+        )
