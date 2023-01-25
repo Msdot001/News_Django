71d9d5e089fe55093c89f2c4f3f7ffb4dfec7b66
Moshood Owolabi <98819100+Msdot001@users.noreply.github.com>
2023-01-25 09:05:26 +0100

    adding CRUD

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from articles.views import ArticleListView
-
-
+from articles.views import (
+    ArticleListView,
+    ArticleDetailView,
+    ArticleUpdateView,
+    ArticleDeleteView,
+)
 urlpatterns = [
     path("", ArticleListView.as_view(), name="article_list"),
+    path("<int:pk>", ArticleDetailView.as_view(), name="article_delete"),
+    path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit"),
+    path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Article
 
 
@@ -6,4 +8,17 @@ class ArticleListView(ListView):
     model = Article
     template_name = "article_list.html"
 
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = "article_detail.html"
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields =("title", "body",)   # field intend to update
+    template_name = "article_update.html"
+
+class ArticleDeleteView(DeleteView):
+    model= Article
+    template_name = "article_delete"
+    success_url = reverse_lazy("article_list")
 
