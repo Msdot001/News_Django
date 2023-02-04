0e5e7347734df9aba1a38e115b00c4a1486ce010
Moshood Owolabi <98819100+Msdot001@users.noreply.github.com>
2023-02-04 15:10:09 +0100

    add Authorization nad Permission

--- articles/urls.py
+++ articles/urls.py
@@ -4,10 +4,12 @@ from articles.views import (
     ArticleDetailView,
     ArticleUpdateView,
     ArticleDeleteView,
+    ArticleCreateView,
 )
 urlpatterns = [
     path("", ArticleListView.as_view(), name="article_list"),
     path("<int:pk>", ArticleDetailView.as_view(), name="article_detail"),
     path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit"),
     path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"),
+    path("new/", ArticleCreateView.as_view(), name="article_new"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,24 +1,57 @@
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
+
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 from .models import Article
 
+"""
+LoginRequiredMixin: Views can only be access by  logged-in user
 
-class ArticleListView(ListView):
+UserPassesTestMixin: view can only be access when authorised user is confirmed
+"""
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = "article_list.html"
 
-class ArticleDetailView(DetailView):
+
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = "article_detail.html"
 
-class ArticleUpdateView(UpdateView):
+
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
-    fields =("title", "body",)   # field intend to update
+    fields =("title", "body",)   
     template_name = "article_edit.html"
 
-class ArticleDeleteView(DeleteView):
+    def test_func(self): 
+        """Only authoruised author can update"""
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model= Article
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
+
+    def test_func(self):
+        """Only authoruised author can update"""
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model= Article
+    template_name = "article_new.html"
+    fields = ("title", "body",)
+
+    def form_valid(self, form): # new
+        """Setting the author to the current logged-in User"""
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
 
